5dd596782af9e67879e66b5b70a7588512def7df
Nancy Emanuel <76671907+NancyEmanuel@users.noreply.github.com>
2024-11-28 17:01:39 -0500

    Update RedisConnectionManager.java

--- Distributed Sytems Proj/WeatherAlertSystem-main/WeatherAlertSystem-main/src/main/java/com/weatheralert/RedisConnectionManager.java
+++ Distributed Sytems Proj/WeatherAlertSystem-main/WeatherAlertSystem-main/src/main/java/com/weatheralert/RedisConnectionManager.java
@@ -10,5 +10,12 @@ public class RedisConnectionManager {
         }
         return jedis;
     }
+
+    public static void closeAllConnections() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'closeAllConnections'");
+    }
 }
+
+
 
